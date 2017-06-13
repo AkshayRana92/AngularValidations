b24420759bc6cf1870d765fed30356d60536f0b5
AkshayRana92 <akshayrana@knoldus.in>
2017-06-13 19:13:34 +0530

    Added validations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
-
+import {Component,
+    AfterViewChecked,
+    ViewChild} from '@angular/core';
+import {NgForm} from '@angular/forms';
+import {AppValidationMessages} from "./app.messages";
 @Component({
   selector: 'my-app',
   moduleId: module.id,
@@ -17,19 +20,30 @@ import { Component } from '@angular/core';
 
     <div class="row">
       <div class="col-md-4 col-md-offset-4">
-        <input type="text" name="email" [(ngModel)]="email" class="form-control" placeholder="email"/>
+        <input type="email" email required name="email" [(ngModel)]="email" class="form-control" placeholder="email"/>
+      </div>
+      <div class="col-md-4 col-md-offset-4">
+        <div *ngIf="formErrors.email" class="alert alert-danger">{{formErrors.email}}</div>
       </div>
     </div>
 
     <div class="row">
       <div class="col-md-4 col-md-offset-4">
-        <input type="text" name="password" [(ngModel)]="password" class="form-control" placeholder="password"/>
+        <input type="password" minlength="8" required name="password" [(ngModel)]="password" class="form-control" placeholder="password"/>
+      </div>
+      <div class="col-md-4 col-md-offset-4">
+        <div *ngIf="formErrors.password" class="alert alert-danger">{{formErrors.password}}</div>
       </div>
     </div>
 
+
     <div class="row">
       <div class="col-md-4 col-md-offset-4">
-        <button class="btn btn-primary" type="submit">Submit</button>
+
+        <div *ngIf="myForm.form.valid;then formValid else formError"></div>
+        <ng-template #formValid><button class="btn btn-primary" type="submit">Submit</button></ng-template>
+        <ng-template #formError><button class="btn btn-primary" type="button" (click)="submitInvalidForm()">Submit</button></ng-template>
+
       </div>
     </div>
 
@@ -40,7 +54,14 @@ import { Component } from '@angular/core';
   styles: ['form{ margin : 100px}', '.row{ margin-bottom : 10px}','button{width: 100%}']
 })
 
-export class AppComponent  {
+export class AppComponent implements AfterViewChecked{
+  formErrors = {
+    'email': '',
+    'password': ''
+  };
+
+  myForm: NgForm;
+  @ViewChild('myForm') currentForm: NgForm;
 
   email: string = '';
   password: string = '';
@@ -49,4 +70,51 @@ export class AppComponent  {
     alert('email : ' + this.email + '\npass : ' + this.password)
   }
 
+  ngAfterViewChecked() {
+    this.formChange();
+  }
+
+  formChange() {
+    if (this.currentForm === this.myForm) { return; }
+
+    this.myForm = this.currentForm;
+    if (this.myForm) {
+      this.myForm.valueChanges
+          .subscribe(data => this.onValueChanged());
+    }
+  }
+
+  onValueChanged() {
+    if (!this.myForm) { return; }
+    const form = this.myForm.form;
+
+    for (const field in this.formErrors) {
+      this.formErrors[field] = '';
+      const control = form.get(field);
+
+      if (control && control.dirty && !control.valid) {
+        const messages = AppValidationMessages.errorMessages[field];
+        for (const key in control.errors) {
+          this.formErrors[field] = messages[key];
+        }
+      }
+    }
+  }
+
+  submitInvalidForm() {
+    if (!this.myForm) { return; }
+    const form = this.myForm.form;
+
+    for (const field in this.formErrors) {
+      this.formErrors[field] = '';
+      const control = form.get(field);
+
+      if (control && !control.valid) {
+        const messages = AppValidationMessages.errorMessages[field];
+        for (const key in control.errors) {
+          this.formErrors[field] = messages[key];
+        }
+      }
+    }
+  }
 }
